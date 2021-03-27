ceda5db37bae01c02977afa36681cb47834db23b
Xiaomeng Nie <ca_niexiaomeng@126.com>
2021-03-28 01:57:28 +0800

    phase3 mainpage2 finished

--- src/main/ui/FoodListPage.java
+++ src/main/ui/FoodListPage.java
@@ -2,7 +2,16 @@ package ui;
 
 import model.FoodList;
 
-public class FoodListPage {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FoodListPage extends JFrame implements ActionListener {
     public FoodListPage(FoodList foodList) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
